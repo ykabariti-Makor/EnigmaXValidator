3ae14bae603a7dfd05b8e4244273044b7096cdcf
mtal-socher-makor <mtal-socher@makor-capital.com>
2022-02-06 16:07:29 +0200

    tagSplitter seem to work

--- tagChecker.js
+++ tagChecker.js
@@ -1,10 +1,10 @@
-function tagChecker(str){
-    let regSeparator = / /;
-    let tags = [];
-   
-    if(str.search(regSepartor)){
+function tagSplitter(str){
 
-    }
-  
+    //the separator is either white space of any type or a comma
+    let regSeparator = /\s|,/; 
+    tags = str.split(regSeparator);
+    //otherwise, empty strings might be returned as words - space after comma results in that
+    filteredTags = tags.filter(item => item !== ""); 
+    return filteredTags;
     
 }
